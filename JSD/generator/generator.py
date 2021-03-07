39498f395336d9c3ca2f6ea0b4079737a75d1eec
OljaSekulic <olja.sekulic.77@gmail.com>
2021-03-07 16:26:45 +0100

    Added typescript template for preview models Modified generator file Added component to app module Added component to app routing Added previewTypescript.js file

--- JSD/generator/generator.py
+++ JSD/generator/generator.py
@@ -145,6 +145,10 @@ def generate(model, output_path, overwrite):
         f = open(join(component_folder, "%sPreview.html" % model.name), 'w')
         f.write(template.render(model=model, datetime=now))
 
+        template = jinja_env.get_template('previewTypescript.j2')
+        f = open(join(component_folder, "%sPreview.ts" % model.name), 'w')
+        f.write(template.render(model=model, datetime=now))
+
         
 
  
